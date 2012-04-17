f571b16792e0f1edaf10b0f9b71e1264c383154a
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-04-18 00:09:58 +0200

    Renamed

--- src/de/friedenhagen/xsltproc/XsltProc.java
+++ src/de/friedenhagen/xsltproc/XsltProc.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication3;
+package de.friedenhagen.xsltproc;
 
 import java.io.File;
 import java.io.FileInputStream;
