b090156f5e356e4353364d82d533881cc991b7bd
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-04-18 00:05:56 +0200

    Initial

---
+++ src/de/friedenhagen/xsltproc/XsltProc.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ *
+ * @author mirko
+ */
+public class XsltProc {
+
+    final static String ENCODING = System.getProperty("encoding", "utf-8");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws TransformerConfigurationException, TransformerException, UnsupportedEncodingException, FileNotFoundException {
+        final TransformerFactory factory = TransformerFactory.newInstance();
+        final StreamSource styleSheet = new StreamSource(new File(args[0]));
+        final Transformer transformer = factory.newTransformer(styleSheet);
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        final StreamSource inputXml = new StreamSource(new InputStreamReader(new FileInputStream(args[1]), ENCODING));
+        transformer.transform(inputXml, new StreamResult(System.out));
+    }
+}
